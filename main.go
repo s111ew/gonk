442e7bbb0959ae5178da3876df106142009ee1b7
Sam llewellyn <167631312+s111ew@users.noreply.github.com.>
2025-11-28 18:08:27 +0000

    feat: add funcs to enable and disable 'no-echo' mode in user terminal

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+
+	terminalconfig "github.com/s111ew/gonk/terminal_config"
+)
 
 func main() {
+	err := terminalconfig.EnableRawMode()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer func() {
+		if err := terminalconfig.DisableRawMode(); err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to restore terminal:", err)
+		}
+	}()
+
 	var buf [1]byte
 	for {
 		n, err := os.Stdin.Read(buf[:])
---
+++ terminal_config/terminal_config.go
@@ -0,0 +1,46 @@
+package terminalconfig
+
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+// save a reference to the user's terminal settings
+// so we can restore them when we exit the program
+var origTermios *unix.Termios
+
+// disable ECHO mode in the terminal
+// (user can't see what is typed in the terminal)
+func EnableRawMode() error {
+	// grab a reference to the os' standard input stream
+	// (where we read the terminal input from)
+	fd := int(os.Stdin.Fd())
+
+	// fetch current terminal settings
+	termios, err := unix.IoctlGetTermios(fd, unix.TIOCGETA)
+	if err != nil {
+		return err
+	}
+
+	origTermios = termios
+
+	// clear the setting for 'terminal echo'
+	raw := *termios
+	raw.Lflag &^= unix.ECHO
+
+	// flush any pending input before applying changes
+	err = unix.IoctlSetTermios(fd, unix.TIOCSETAF, &raw)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// re-enable ECHO mode in the terminal
+func DisableRawMode() error {
+	fd := int(os.Stdin.Fd())
+	err := unix.IoctlSetTermios(fd, unix.TIOCSETAF, origTermios)
+	return err
+}
